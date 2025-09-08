4a0f09e6076af5069b7cc7285f4290103d622d12
MaikSa2 <maik.sauck@t-online.de>
2025-09-08 15:34:48 +0200

    run variables

---
+++ scripts/aa_run_variables.py
@@ -0,0 +1,5 @@
+
+
+eur_file =  r"/home/student_01/Student_Folders/Maik/pypsa-eur/resources/a1_base_shipping_ME_SH2/networks/base_s_20__3h_2050.nc" #r"/home/student_01/Student_Folders/Maik/pypsa-eur/resources/06/networks/base_s_2___2050.nc"
+
+
