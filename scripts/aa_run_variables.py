77b1663e5c3f21ba9e18b0d94628cd48e42c62c7
MaikSa2 <maik.sauck@t-online.de>
2025-10-26 15:38:55 +0100

    flexible fedifnition of eur_file

--- scripts/aa_run_variables.py
+++ scripts/aa_run_variables.py
@@ -1,5 +1,23 @@
+import yaml
+from pathlib import Path
+
+# Config laden
+with open(r"/home/student_01/Student_Folders/Maik/pypsa-eur/config/config_a5.yaml") as f:  # Pfad anpassen
+    config = yaml.safe_load(f)
+
+prefix = config["run"]["prefix"]
+
+HERE = Path(__file__).resolve()
+REPO_ROOT = HERE.parent.parent 
+
+eur_file =(REPO_ROOT  / "resources" / prefix / "networks" / "base_s_20__3h_2050.nc").resolve()
+   #print(config)
+print(type(config))
+print(prefix)
+print(eur_file)
 
 
-eur_file =  r"/home/student_01/Student_Folders/Maik/pypsa-eur/resources/a1_base_shipping_ME_SH2/networks/base_s_20__3h_2050.nc" #r"/home/student_01/Student_Folders/Maik/pypsa-eur/resources/06/networks/base_s_2___2050.nc"
+
+   #eur_file =  r"/home/student_01/Student_Folders/Maik/pypsa-eur/resources/a3_base_shipping/networks/base_s_20__3h_2050.nc" #r"/home/student_01/Student_Folders/Maik/pypsa-eur/resources/06/networks/base_s_2___2050.nc"
 
 
